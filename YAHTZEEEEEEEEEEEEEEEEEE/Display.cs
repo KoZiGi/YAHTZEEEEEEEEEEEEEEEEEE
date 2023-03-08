20c4428b2589add75d7d5cc07d9801a0e07e0540
zickbalazs <zickbalazs@turr.hu>
2023-03-08 09:07:05 +0100

    A

--- YAHTZEEEEEEEEEEEEEEEEEE/Display.cs
+++ YAHTZEEEEEEEEEEEEEEEEEE/Display.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace YAHTZEEEEEEEEEEEEEEEEEE
 {
     class Display
     {
+
+        public static void diceClick(Label diceLbl)
+        {
+            if (diceLbl.BackColor == Color.PowderBlue)
+            {
+                diceLbl.BackColor = Color.HotPink;
+            }
+            else
+            {
+                diceLbl.BackColor = Color.PowderBlue;
+            }
+        }
+
+
     }
 }
--- YAHTZEEEEEEEEEEEEEEEEEE/Form1.cs
+++ YAHTZEEEEEEEEEEEEEEEEEE/Form1.cs
@@ -15,6 +15,15 @@ namespace YAHTZEEEEEEEEEEEEEEEEEE
         public Form1()
         {
             InitializeComponent();
+            setUpDisplayFunctions();
+        }
+        private void setUpDisplayFunctions()
+        {
+            Dice1.Click += delegate { Display.diceClick(Dice1); };
+            Dice2.Click += delegate { Display.diceClick(Dice2); };
+            Dice3.Click += delegate { Display.diceClick(Dice3); };
+            Dice4.Click += delegate { Display.diceClick(Dice4); };
+            Dice5.Click += delegate { Display.diceClick(Dice5); };
         }
     }
 }
