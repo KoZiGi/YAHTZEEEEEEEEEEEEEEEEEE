94f9ee241f3e23533e1df2753a89c41b1bfdbbcb
zickbalazs <zickbalazs@turr.hu>
2023-03-08 08:41:03 +0100

    asdfghjkléá

---
+++ YAHTZEEEEEEEEEEEEEEEEEE/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAHTZEEEEEEEEEEEEEEEEEE
+{
+    class Player
+    {
+        public string Name;
+        private Scores playerScore = new Scores();
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+}
